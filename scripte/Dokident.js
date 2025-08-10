216d406fce11c91834f9402386b55a183c8d491b
I-I-Rubin <53793941+I-I-Rubin@users.noreply.github.com>
2025-08-09 20:17:05 -0400

    content from old mlwerke.de/me

---
+++ scripte/Dokident.js
@@ -0,0 +1,11 @@
+﻿
+function DateiInfo() {
+	var Zeitstempel = new Date(document.lastModified);
+	if (Zeitstempel != 0) {
+		document.write ("<p style='font-size: x-small;'>Dokument &raquo;" + document.URL + "&laquo; zuletzt ge&auml;ndert am " + Zeitstempel.toLocaleDateString() + " um " + Zeitstempel.toLocaleTimeString() + "</p>");
+			};
+	};
+ 
+
+
+
